87f52dde7a3a3bc1ae47ac031b326337ee112469
Ilya Galuzinskii <IlyaGall01@gmail.com>
2025-01-09 16:53:52 +0300

    2

---
+++ ProfHomeWork2/ProfHomeWork1/NumberOfSpaces.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfHomeWork1
+{
+    internal class NumberOfSpaces
+    {
+        /// <summary>
+        /// Метод для работы с файлом
+        /// </summary>
+        /// <param name="filePath">Путь к файлу</param>
+        private int OpenTxtAndGetCountSpace(string filePath) 
+        {
+            if (File.Exists(filePath))
+            {
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    return reader.ReadToEnd().Count(x => x == ' ');
+                }
+            }
+            else 
+            {
+                throw new Exception("Файла по указаному путь не существует");
+            }
+        }
+
+        /// <summary>
+        /// Получить колличество пробелов в файле
+        /// </summary>
+        /// <param name="path">Путь к папке</param>
+        /// <returns></returns>
+        public int GetSpace(string path) 
+        {
+            OpenTxtAndGetCountSpace(path);
+            return 0;
+        }
+    }
+}
---
+++ ProfHomeWork2/ProfHomeWork1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ProfHomeWork1
+{
+
+    /*
+     Описание/Пошаговая инструкция выполнения домашнего задания:
+     Прочитать 3 файла параллельно и вычислить количество пробелов в них (через Task).
+     Написать функцию, принимающую в качестве аргумента путь к папке. Из этой папки параллельно прочитать все файлы и вычислить количество пробелов в них.
+     Замерьте время выполнения кода (класс Stopwatch).
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NumberOfSpaces numberOfSpaces = new NumberOfSpaces();
+            numberOfSpaces.GetSpace(@"C:\\Users\\Ilya\\Desktop\\Новая папка (54)\\Новый текстовый документ (2).txt");
+
+            Task task = new Task(() => Console.WriteLine("Hello Task!"));
+            task.Start();
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
